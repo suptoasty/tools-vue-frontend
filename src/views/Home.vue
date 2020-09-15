8db8472a7fb667cc41a0ad95ed801614e33ed9c6
Lane Simpson <lsimpson1040@gmail.com>
2020-09-14 22:12:22 -0500

    Added skeleton of home page - no functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,56 @@
     class="fill-height"
     fluid
   >
+
     <v-row
       align="center"
       justify="center"
     >
       <v-col
-        cols="2"
-        sm="5"
-        md="5"
+        cols="5"
       >
-        <ClassListing :classObject="{ name: 'SEIV class' }"/>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <h1 class="text-center h1">Course List</h1>
+        </v-row>
+        <v-row>
+            <v-text-field
+            label="Search"
+            outlined
+            >
+            </v-text-field>
+            <v-overflow-btn
+              class="my-n2"
+              :items="searchOptions"
+              label="Search By"
+            ></v-overflow-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="5"
+      >
+        <ClassListing :classObject="{ name: 'SEIV class', description: 'A lot of things happen' }"/>
+        <ClassListing :classObject="{ name: 'SEIV class', description: 'A lot of things happen' }"/>
+      </v-col>
+    </v-row>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col>
+        <v-pagination
+          :length="4"
+          v-model="page"
+          circle
+        ></v-pagination>
       </v-col>
     </v-row>
   </v-container>
@@ -26,6 +66,17 @@ export default {
   name: "Home",
   components: {
     ClassListing
-  }
+  },
+  data: () => ({
+    searchOptions: [
+      'Name',
+      'Number',
+      'Semesters Offered',
+      'Credit Hours',
+      'Field of Study',
+      'Class Level'
+    ],
+    page: 1
+  })
 };
 </script>
