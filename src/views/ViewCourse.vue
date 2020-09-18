5465197e41ef262f3d7050326f29597364d202d8
ryanburnett1.ryan <ryanburnett1.ryan.burnett@eagles.oc.edu>
2020-09-17 21:23:45 -0500

    created layout for display course page

---
+++ src/views/ViewCourse.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container
+    fluid
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="2"
+        sm="5"
+        md="5"
+      >
+        <DisplayClass :classObject="{ name: 'SEIV class' }"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import DisplayClass from "@/components/DisplayClass.vue";
+
+export default {
+  name: "ViewCourse",
+  components: {
+    DisplayClass
+  }
+};
+</script>
