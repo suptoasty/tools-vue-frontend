72392d56c5781a05325b72d7f3e5b419996c96c6
suptoasty <jason.lonsinger@gmail.com>
2020-09-18 09:54:24 -0500

    Fixed Axios baseURL

--- src/components/ClassListing.vue
+++ src/components/ClassListing.vue
@@ -1,61 +1,34 @@
 <template>
-    <v-card
-    class="elevation-3 mb-4"
-    height=150
-    style="overflow: hidden"
-    >
-        <v-toolbar
-        color="primary"
-        dark
-        flat
-        >
-            <v-toolbar-title @click="log('hello')"
-            >
-                <v-btn
-                small
-                text
-                :ripple="false"
-                link
-                >
-                    <v-icon style="font-style:normal">{{classObject.course_name}}</v-icon>
-                </v-btn>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-tooltip bottom>
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                    small
-                    text
-                    fab
-                    v-bind="attrs"
-                    v-on="on"
-                    >
-                        <v-icon>mdi-pencil</v-icon>
-                    </v-btn>
-                </template>
-                <span>Edit</span>
-            </v-tooltip>
-            <v-tooltip bottom>
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                    small
-                    text
-                    fab
-                    v-bind="attrs"
-                    v-on="on"
-                    >
-                        <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                </template>
-                <span>Delete</span>
-            </v-tooltip>
-        </v-toolbar>
-        <v-card-text>
-            <p>{{classObject.course_desc}}</p>
-        </v-card-text>
-        <v-card-actions>
-        </v-card-actions>
-    </v-card>
+  <v-card class="elevation-3 mb-4" height="150" style="overflow: hidden">
+    <v-toolbar color="primary" dark flat>
+      <v-toolbar-title @click="log('hello')">
+        <v-btn small text :ripple="false" link>
+          <v-icon style="font-style:normal">{{classObject.course_name}}</v-icon>
+        </v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn small text fab v-bind="attrs" v-on="on">
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </template>
+        <span>Edit</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn small text fab v-bind="attrs" v-on="on">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </template>
+        <span>Delete</span>
+      </v-tooltip>
+    </v-toolbar>
+    <v-card-text>
+      <p>{{classObject.course_desc}}</p>
+    </v-card-text>
+    <v-card-actions></v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -63,14 +36,13 @@ export default {
   name: "ClassListing",
 
   props: {
-      classObject: Object
+    classObject: Object
   },
-  data: () => ({
-  }),
+  data: () => ({}),
   methods: {
-      log( message ) {
-          console.log(message);
-      }
+    log(message) {
+      console.log(message);
+    }
   }
 };
 </script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {
+  name: "Navigation",
+
+  props: {
+    classObject: Object
+  },
+  data: () => ({}),
+  methods: {
+    log(message) {
+      console.log(message);
+    }
+  }
+};
+</script>
