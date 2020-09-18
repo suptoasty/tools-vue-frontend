e5feadbedbc2d28bdd0d10dcbf8119683d20bee8
ryanburnett1.ryan <ryanburnett1.ryan.burnett@eagles.oc.edu>
2020-09-18 10:08:00 -0500

    pushing routing page

--- src/router/index.js
+++ src/router/index.js
@@ -6,26 +6,14 @@ import ViewCourse from "../views/ViewCourse.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  /*{
-    path: "/",
-    name: "Home",
-    component: Home
-  },*/
+  
   {
     path: "/class/:courseIndex",
     name: "ViewCourse",
     component: ViewCourse,
     props: true
   },
-  /*{
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about"  "../views/About.vue")
-  }*/
+  
 ];
 
 const router = new VueRouter({
