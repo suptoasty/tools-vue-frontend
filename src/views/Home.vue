c228060bdb4e62c09662d453890ecb945449e9bc
Lane Simpson <lsimpson1040@gmail.com>
2020-09-22 20:33:00 -0500

    Finish view classes page functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,90 @@
 <template>
   <v-container class="fill-height" fluid>
     <v-row align="center" justify="center">
-      <v-col cols="5">
+      <v-col cols="7">
         <v-row align="center" justify="center">
           <h1 class="text-center h1">Course List</h1>
         </v-row>
+        <v-row align="center" justify="center">
+          <v-btn color="primary" @click="$router.push({ name: 'AddCourse' })">Add Course</v-btn>
+        </v-row>
         <v-row>
-          <v-text-field label="Search" outlined></v-text-field>
-          <v-overflow-btn class="my-0" :items="searchOptions" label="Search By"></v-overflow-btn>
+          <v-col cols="12">
+            <v-text-field
+              label="Search Courses"
+              v-model="search"
+            >
+            </v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-select
+              v-model="includeInSearch"
+              :items="searchOptions"
+              chips
+              label="Search By"
+              multiple
+            >
+            <template v-slot:selection="{ item }">
+              <v-chip>
+                <span>{{ item }}</span>
+              </v-chip>
+            </template>
+            </v-select>
+          </v-col>
         </v-row>
         <v-row align="center" justify="center">
-          <v-data-table :headers="headers" :items="classes" :items-per-page="5" class="elevation-1">
-            <template v-slot:item="row">
+          <v-data-table
+            :headers="headers"
+            :items="classes"
+            :items-per-page="10"
+            :search="search"
+            class="elevation-1"
+          >
+            <template v-slot:item.actions="{ item }">
               <tr>
                 <td>
-                  <ClassListing :classObject="row.item" />
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        small
+                        text
+                        fab
+                        v-bind="attrs"
+                        v-on="on"
+                        @click="$router.push({ name: 'ViewCourse', params: {courseIndex: item.course_id} })"
+                      >
+                        <v-icon>mdi-book-open-variant</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>View</span>
+                  </v-tooltip>
+                </td>
+                <td>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        small
+                        text
+                        fab
+                        v-bind="attrs"
+                        v-on="on"
+                        @click="$router.push({ name: 'EditCourse', params: {courseIndex: item.course_id} })"
+                      >
+                        <v-icon>mdi-pencil</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Edit</span>
+                  </v-tooltip>
+                </td>
+                <td>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn small text fab v-bind="attrs" v-on="on" @click="onDelete(item)">
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Delete</span>
+                  </v-tooltip>
                 </td>
               </tr>
             </template>
@@ -29,29 +99,25 @@
 <script>
 // @ is an alias to /src
 import CourseService from "@/services/CourseService.js";
-import ClassListing from "@/components/ClassListing.vue";
 import DeleteConfirmation from "@/components/DeleteConfirmation.vue";
 
 export default {
   name: "Home",
   components: {
-    ClassListing,
     DeleteConfirmation
   },
   data: () => ({
-    searchOptions: [
-      "Name",
-      "Number",
-      "Semesters Offered",
-      "Credit Hours",
-      "Field of Study",
-      "Class Level"
-    ],
-    headers: [{ text: "Name", value: "course_name" }],
+    search: "",
+    searchOptions: ["Name", "Department", "Number", "Level", "Description"],
+    includeInSearch: ["Name", "Department", "Number", "Level", "Description"],
     classes: [],
     page: 1
   }),
   methods: {
+    onDelete(course) {
+      console.log("Emiting Delete for: " + course.course_name);
+      this.$root.$emit("deleteCourse", course);
+    }
     // onSort: function(items, index, isDesc) {
     //   items.sort((a, b) => {
     //     console.log(isDesc);
@@ -61,6 +127,19 @@ export default {
     //   });
     //   return items;
     // }
+  },
+  computed: {
+      headers () {
+        return [
+          { text: "Name", value: "course_name", filterable: this.includeInSearch.includes("Name") },
+          { text: "Department", value: "course_dept", filterable: this.includeInSearch.includes("Department") },
+          { text: "Number", value: "course_num", filterable: this.includeInSearch.includes("Number") },
+          { text: "Level", value: "course_level", filterable: this.includeInSearch.includes("Level") },
+          { text: "Hours", value: "course_hours", filterable: this.includeInSearch.includes("Hours") },
+          { text: "Description", value: "course_desc", filterable: this.includeInSearch.includes("Description") },
+          { text: "Actions", value: "actions" },
+        ]
+      }
   },
   created() {
     CourseService.getCourses()
