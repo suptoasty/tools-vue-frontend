6643cd833e13ecd325a9c0242fcdc6aed395917b
Nathan Woodruff <nathan.woodruff@eagles.oc.edu>
2020-09-26 00:55:20 -0500

    Changed margins for course edit page

--- src/components/EditingCourse.vue
+++ src/components/EditingCourse.vue
@@ -1,77 +1,81 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col>
-                <h1>Edit Course</h1>
-            </v-col>
-        </v-row>
-        <v-divider></v-divider>
-        <v-row>
-            <v-col>
-                <p>Course name:</p>
-            </v-col>
-            <v-col>
-                <v-text-field
-                v-model = "courseObject.course_name"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <p>Course Number:</p>
-            </v-col>
-            <v-col>
-                <v-text-field
-                v-model="courseObject.course_num"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <p>Description:</p>
-            </v-col>
-            <v-col>
-                <v-text-field
-                v-model = "courseObject.course_desc"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <p>Course Hours:</p>
-            </v-col>
-            <v-col>
-                <v-text-field
-                v-model = "courseObject.course_hours"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <p>Course Department:</p>
-            </v-col>
-            <v-col>
-                <v-text-field
-                v-model = "courseObject.course_dept"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <p>Class Level:</p>
-            </v-col>
-            <v-col>
-                <v-text-field
-                v-model = "courseObject.course_level"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col align = "center">
-                <v-btn v-on:click.native = "saveClass" color="primary">Save</v-btn>
-            </v-col>
-            <v-col align = "center">
-                <v-btn color="primary" :to="{ name: this.returnTo, params: { courseIndex: courseObject.course_id }}">Cancel</v-btn>
+    <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+            <v-col cols="7">
+                <v-row>
+                    <v-col>
+                        <h1>Edit Course</h1>
+                    </v-col>
+                </v-row>
+                <v-divider></v-divider>
+                <v-row>
+                    <v-col>
+                        <p>Course name:</p>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        v-model = "courseObject.course_name"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <p>Course Number:</p>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        v-model="courseObject.course_num"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <p>Description:</p>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        v-model = "courseObject.course_desc"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <p>Course Hours:</p>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        v-model = "courseObject.course_hours"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <p>Course Department:</p>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        v-model = "courseObject.course_dept"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col>
+                        <p>Class Level:</p>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        v-model = "courseObject.course_level"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col align = "center">
+                        <v-btn v-on:click.native = "saveClass" color="primary">Save</v-btn>
+                    </v-col>
+                    <v-col align = "center">
+                        <v-btn color="primary" :to="{ name: this.returnTo, params: { courseIndex: courseObject.course_id }}">Cancel</v-btn>
+                    </v-col>
+                </v-row>
             </v-col>
         </v-row>
     </v-container>
