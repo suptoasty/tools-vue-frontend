14dbaecd82a7ab5a3038097eca49a98d9d1c4861
Lane Simpson <lsimpson1040@gmail.com>
2020-10-12 20:35:12 -0500

    added new routes to CRUD calls. still not finished

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -16,6 +16,10 @@ const apiClient = axios.create({
 
 
 export default {
+
+  //---------------------
+  //courses
+  //---------------------
   getCourses() {
     return apiClient.get("/courseapi/courses/");
   },
@@ -45,4 +49,99 @@ export default {
       course_hours: course.course_hours
     });
   },
+  //---------------------
+  //advisors
+  //---------------------
+  getAdvisors() {
+    return apiClient.get("/courseapi/advisor/");
+  },
+  getAdvisor(id) {
+    return apiClient.get("/courseapi/advisor/" + id);
+  },
+  postAdvisor(advisor) {
+    return apiClient.post("/courseapi/advisor", {
+      advisor_department: advisor.advisor_department,
+      advisor_fname: advisor.advisor_fname,
+      advisor_id: advisor.advisor_id,
+      advisor_initial: advisor.advisor_initial,
+      advisor_lname: advisor.advisor_lname
+    });
+  },
+  deleteAdvisor(id) {
+    return apiClient.delete("/courseapi/advisor/" + id);
+  },
+  putAdvisor(id, advisor) {
+    return apiClient.put("/courseapi/advisor/" + id, {
+      advisor_department: advisor.advisor_department,
+      advisor_fname: advisor.advisor_fname,
+      advisor_id: advisor.advisor_id,
+      advisor_initial: advisor.advisor_initial,
+      advisor_lname: advisor.advisor_lname
+    });
+  },
+  //---------------------
+  //degrees
+  //---------------------
+  getDegrees() {
+    return apiClient.get("/courseapi/degrees/");
+  },
+  getDegree(id) {
+    return apiClient.get("/courseapi/degrees/" + id);
+  },
+  postDegree(degree) {
+    return apiClient.post("/courseapi/degrees", {
+      degree_dept: degree.degree_dept,
+      degree_hours: degree.degree_hours,
+      degree_id: degree.degree_id,
+      degree_name: degree.degree_name
+    });
+  },
+  deleteDegree(id) {
+    return apiClient.delete("/courseapi/degrees/" + id);
+  },
+  putDegree(id, degree) {
+    return apiClient.put("/courseapi/degrees/" + id, {
+      degree_dept: degree.degree_dept,
+      degree_hours: degree.degree_hours,
+      degree_id: degree.degree_id,
+      degree_name: degree.degree_name
+    });
+  },
+  //---------------------
+  //degree plans
+  //---------------------
+  getDegreePlans() {
+    return apiClient.get("/courseapi/degree-plans/");
+  },
+  getDegreePlan(id) {
+    return apiClient.get("/courseapi/degree-plans/" + id);
+  },
+  postDegreePlan(degreePlan) {
+    return apiClient.post("/courseapi/degree-plans", {
+      degree_plan_id: degreePlan.degree_plan_id,
+      degree: degreePlan.degree,
+      course: degreePlan.course
+    });
+  },
+  deleteDegreePlan(id) {
+    return apiClient.delete("/courseapi/degree-plans/" + id);
+  },
+  putDegreePlan(id, degreePlan) {
+    return apiClient.put("/courseapi/degree-plans/" + id, {
+      degree_plan_id: degreePlan.degree_plan_id,
+      degree: degreePlan.degree,
+      course: degreePlan.course
+    });
+  },
+  //---------------------
+  //TODO: course plans
+  //---------------------
+
+  //---------------------
+  //TODO: semesters
+  //---------------------
+
+  //---------------------
+  //TODO: something else?
+  //---------------------
 };
