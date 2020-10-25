b027c0686241feb41daf44a9ff58fdb0380da16e
Lane Simpson <lsimpson1040@gmail.com>
2020-10-24 22:36:01 -0500

    refined interface. added course details

--- src/views/CoursePlan.vue
+++ src/views/CoursePlan.vue
@@ -19,7 +19,7 @@
                 <v-spacer></v-spacer>
 
                 <v-toolbar-title>
-                  Total Hours: X
+                  {{"Total Hours: " + totalHours[index]}}
                 </v-toolbar-title>
                 <v-icon>
                   {{semesterShow[index] ? "mdi-arrow-up-drop-circle" : "mdi-arrow-down-drop-circle"}}
@@ -28,16 +28,59 @@
             </v-card>
           </v-col>
         </v-row>
-        <v-data-table
-          hide-default-footer
-          v-show="semesterShow[index]"
-          :headers="headers"
-          :items="semesterItems"
-          item-key="course_plan_item_id"
-        >
-        </v-data-table>
-        <v-divider></v-divider>
+        <v-row justify="center" v-show="semesterShow[index]">
+          <v-col cols="10">
+            <v-btn
+              color="blue lighten-1 white--text"
+              class="mb-8 ml-3"
+              outlined
+              height="40"
+              x-large
+              right
+              @click="addCoursePlanItem(index)"
+              >Add Course</v-btn>
+            <v-data-table
+              hide-default-footer
+              :headers="headers"
+              :items="semesterItems"
+              item-key="course_plan_item_id"
+            >
+              <template v-slot:item.actions="{ item }">
+                <tr>
+                  <td>
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-btn
+                            small
+                            text
+                            fab
+                            v-bind="attrs"
+                            v-on="on"
+                            @click="removeCoursePlanItem(index, item)"
+                          >
+                            <v-icon color="red darken-2">mdi-delete</v-icon>
+                          </v-btn>
+                        </template>
+                        <span>Delete</span>
+                      </v-tooltip>
+                  </td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
       </v-col>
+    </v-row>
+    <v-row>
+      <v-btn
+          color="blue lighten-1 white--text"
+          class="mb-8 ml-3"
+          outlined
+          height="40"
+          x-large
+          right
+          @click="addCourseGlobal()"
+          >Add Course</v-btn>
     </v-row>
   </v-container>
 </template>
@@ -60,9 +103,11 @@ export default {
   },
   data: () => ({
     coursePlan: {},
+    totalHours: [],
     sortedCoursePlanItems: [],
     usedSemesters: [],
     allSemesters: [],
+    allCourses: [],
     semesterShow: [],
   }),
   methods: {
@@ -85,18 +130,19 @@ export default {
       this.sortedCoursePlanItems.pop();
       this.semesterShow[index] = !this.semesterShow[index];
     },
-    // addUsedSemester(allSemestersIndex) {
-    //   //update usedSemesters
-    // },
-    // removeUsedSemester(usedSemestersIndex) {
-    //   //update usedSemesters
-    // },
-    // addCoursePlanItem(UsedSemestersIndex) {
-    //   //update sortedCoursePlanItems
-    // },
-    // removeCoursePlanItem(usedSemestersIndex, usedItemsIndex) {
-    //   //update sortedCoursePlanItems
-    // },
+    addCourseGlobal() {
+      //update usedSemesters
+      console.log("add course global");
+    },
+    addCoursePlanItem(usedSemestersIndex) {
+      //update sortedCoursePlanItems
+      console.log(usedSemestersIndex);
+    },
+    removeCoursePlanItem(usedSemestersIndex, item) {
+      //update sortedCoursePlanItems
+      let usedItemsIndex = this.sortedCoursePlanItems[usedSemestersIndex].indexOf(item);
+      console.log(usedItemsIndex);
+    },
     //call this to sort allCoursePlanItems into an array of arrays for each usedSemester
     buildSemesterCoursePlanItems(allCoursePlanItems) {
       //filter semesters by if their id is included in the list of course plan items
@@ -126,23 +172,49 @@ export default {
             if (!foundMatch) {
               index++;
               this.semesterShow.push(false);
+              this.totalHours.push(0);
               this.sortedCoursePlanItems.push([]);
               foundMatch = true;
             }
+            coursePlanItem.course = this.allCourses.filter( (value) => coursePlanItem.course == value.course_id)[0];
+            this.totalHours[index] += Number(coursePlanItem.course.course_hours);
             this.sortedCoursePlanItems[index].push(coursePlanItem);
           }
         });
       });
+      console.log(this.sortedCoursePlanItems);
     }
   },
   computed: {
     headers () {
       return [
         {
-          text: 'Course Plan Item Id',
+          text: 'Course Name',
           align: 'start',
-          value: 'course_plan_item_id',
+          value: 'course.course_name',
         },
+        {
+          text: 'Course Number',
+          align: 'start',
+          value: 'course.course_num',
+        },
+        //add course times here?
+        {
+          text: 'Credit Hours',
+          align: 'start',
+          value: 'course.course_hours',
+        },
+        {
+          text: 'Grade',
+          align: 'start',
+          value: 'course_plan_item_grade',
+        },
+        {
+          text: 'Status',
+          align: 'start',
+          value: 'course_plan_item_status',
+        },
+        { text: "Actions", value: "actions" },
       ]
     },
   },
@@ -154,9 +226,12 @@ export default {
       CourseService.getCoursePlanItems(this.coursePlan.course_plan_id).then( (responseItems) => {
         //get all semesters
         CourseService.getSemesters().then( (responseSemesters) => {
-           this.allSemesters = responseSemesters.data;
-           //build the semester and course plan items list
-           this.buildSemesterCoursePlanItems(responseItems.data);
+          this.allSemesters = responseSemesters.data;
+          CourseService.getCourses().then( (responseCourses) => {
+            this.allCourses = responseCourses.data;
+            //build the semester and course plan items list
+            this.buildSemesterCoursePlanItems(responseItems.data);
+          });
         });
       });
     });
