f6857e1043180aad761e73861f46210500c39581
ryanburnett1.ryan <ryanburnett1.ryan.burnett@eagles.oc.edu>
2020-10-30 02:08:44 -0500

    finished semester delete

---
+++ src/components/SemesterDeleteConfirmation.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="290">
+    <v-card>
+      <v-card-title class="headline" style="word-break: normal">Delete "{{modalTitle}}"?</v-card-title>
+      <v-card-text>All Contents Will Be Deleted! <br/> This Cannot Be Undone!</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="red darken-1" text @click="onDelete(semesterID)">Delete</v-btn>
+        <v-btn color="primary" text @click="onCancel()">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import CourseService from "@/services/CourseService.js";
+import router from "@/router/index.js";
+
+export default {
+  name: "SemesterDeleteConfirmation",
+  data: () => ({
+    dialog: false,
+    semesterID: null,
+    modalTitle: ""
+  }),
+  methods: {
+    onShowModal(semester) {
+      this.modalTitle = semester.semester_name + ": " + semester.semester_id;
+      this.semesterID = semester.semester_id;
+      this.dialog = true;
+    },
+    async onDelete(id) {
+      console.log("DELETING SEMESTER: " + id);
+      this.dialog = false;
+      await CourseService.deleteSemester(id);
+      await this.$root.$emit('SemesterDeleted');
+      router.push({ name: "ViewSemesters" });
+    },
+    onCancel() {
+      this.dialog = false;
+      this.semesterID = null;
+    }
+  },
+  mounted() {
+    this.$root.$on("deleteSemester", semester => {
+      this.onShowModal(semester);
+    });
+  }
+};
+</script>
